6e00d7aff032a55299aa8f59fe5f3b54b74001a9
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2019-03-05 17:12:47 +0900

    SearchConditionSectionのロード時の対応
    ・Sort設定もDefinition生成時にプロパティがない場合は、除外する。

--- iplass-gem/src/main/java/org/iplass/mtp/impl/view/generic/element/section/MetaSortSetting.java
+++ iplass-gem/src/main/java/org/iplass/mtp/impl/view/generic/element/section/MetaSortSetting.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2013 INFORMATION SERVICES INTERNATIONAL - DENTSU, LTD. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -27,8 +27,8 @@ import org.iplass.mtp.impl.entity.property.ReferencePropertyHandler;
 import org.iplass.mtp.impl.metadata.MetaData;
 import org.iplass.mtp.impl.util.ObjectUtil;
 import org.iplass.mtp.view.generic.NullOrderType;
-import org.iplass.mtp.view.generic.element.section.SortSetting;
 import org.iplass.mtp.view.generic.element.section.SearchConditionSection.ConditionSortType;
+import org.iplass.mtp.view.generic.element.section.SortSetting;
 
 public class MetaSortSetting implements MetaData {
 
@@ -136,8 +136,15 @@ public class MetaSortSetting implements MetaData {
 	}
 
 	public SortSetting currentConfig(EntityContext context, EntityHandler entity) {
+
+		//プロパティ名が取得できない場合はnullを返す
+		String name = convertName(sortKey, context, entity);
+		if (name == null) {
+			return null;
+		}
+
 		SortSetting setting = new SortSetting();
-		setting.setSortKey(convertName(sortKey, context, entity));
+		setting.setSortKey(name);
 		setting.setSortType(sortType);
 		setting.setNullOrderType(nullOrderType);
 		return setting;
