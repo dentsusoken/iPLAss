cf409dac09b3991e201a41ef1517ceaa819a7e26
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2019-03-27 13:58:01 +0900

    fix #161

--- iplass-gem/src/main/java/org/iplass/gem/command/generic/search/SearchContextBase.java
+++ iplass-gem/src/main/java/org/iplass/gem/command/generic/search/SearchContextBase.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2012 INFORMATION SERVICES INTERNATIONAL - DENTSU, LTD. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -749,13 +749,7 @@ public abstract class SearchContextBase implements SearchContext {
 	}
 
 	protected SearchQueryInterrupter getDefaultSearchQueryInterrupter() {
-		return new SearchQueryInterrupter() {
-
-			@Override
-			public SearchQueryContext beforeSearch(RequestContext context, SearchFormView view, Query query) {
-				return new SearchQueryContext(query);
-			}
-		};
+		return new SearchQueryInterrupter() {};
 	}
 
 	private static String resourceString(String key, Object... arguments) {
--- iplass-gem/src/main/java/org/iplass/mtp/view/generic/SearchQueryInterrupter.java
+++ iplass-gem/src/main/java/org/iplass/mtp/view/generic/SearchQueryInterrupter.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2014 INFORMATION SERVICES INTERNATIONAL - DENTSU, LTD. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -51,7 +51,9 @@ public interface SearchQueryInterrupter {
 	 * @deprecated use beforeSearch(RequestContext, SearchFormView, Query, SearchQueryType)
 	 */
 	@Deprecated
-	public SearchQueryContext beforeSearch(RequestContext request, SearchFormView view, Query query);
+	default public SearchQueryContext beforeSearch(RequestContext request, SearchFormView view, Query query) {
+		return new SearchQueryContext(query);
+	}
 
 	/**
 	 * 検索前処理を行います。
