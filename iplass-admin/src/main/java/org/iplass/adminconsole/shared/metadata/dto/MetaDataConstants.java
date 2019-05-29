fdd0d8fac733f5619e90064285d814b4cccaab02
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2019-05-29 15:06:21 +0900

    fix #225

--- iplass-admin/src/main/java/org/iplass/adminconsole/shared/metadata/dto/MetaDataConstants.java
+++ iplass-admin/src/main/java/org/iplass/adminconsole/shared/metadata/dto/MetaDataConstants.java
@@ -32,16 +32,16 @@ public interface MetaDataConstants {
 	public static final String META_CATEGORY_NOTIFICATION			= "Notification";
 
 	/** 名前の正規表現(パスにスラッシュを利用) */
-	public static final String NAME_REG_EXP_PATH_SLASH = "^[0-9a-zA-Z_]+(/[0-9a-zA-Z_-]+)*$";
+	public static final String NAME_REG_EXP_PATH_SLASH = "^[0-9a-zA-Z_][0-9a-zA-Z_-]*(/[0-9a-zA-Z_-]+)*$";
 	/** 名前の正規表現(パスにスラッシュを利用、名前にピリオド含む) */
-	public static final String NAME_REG_EXP_PATH_SLASH_NAME_PERIOD = "^[0-9a-zA-Z_]+(/[0-9a-zA-Z_-]+)*(\\.[0-9a-zA-Z_-]+)*$";
+	public static final String NAME_REG_EXP_PATH_SLASH_NAME_PERIOD = "^[0-9a-zA-Z_][0-9a-zA-Z_-]*(/[0-9a-zA-Z_-]+)*(\\.[0-9a-zA-Z_-]+)*$";
 	/** 名前の正規表現(パスにピリオドを利用) */
-	public static final String NAME_REG_EXP_PATH_PERIOD = "^[0-9a-zA-Z_]+(\\.[0-9a-zA-Z_-]+)*$";
+	public static final String NAME_REG_EXP_PATH_PERIOD = "^[0-9a-zA-Z_][0-9a-zA-Z_-]*(\\.[0-9a-zA-Z_-]+)*$";
 
 	/** Entity名の正規表現(英数、アンダスコア、パスにピリオド、先頭の数字不可、マイナス不可) */
 	public static final String ENTITY_NAME_REG_EXP_PATH_PERIOD = "^[a-zA-Z_][0-9a-zA-Z_]*(\\.[a-zA-Z_][0-9a-zA-Z_]*)*$";
 	/** Entityプロパティ名の正規表現(英数、アンダスコア、先頭の数字不可、マイナス不可) */
-	public static final String ENTITY_PROPERTY_NAME_REG_EXP_PATH_PERIOD = "^[a-zA-Z_]+([0-9a-zA-Z_]+)*$";
+	public static final String ENTITY_PROPERTY_NAME_REG_EXP_PATH_PERIOD = "^[a-zA-Z_][0-9a-zA-Z_]*$";
 
 	/** 標準FormItemの幅 */
 	public static final int DEFAULT_FORM_ITEM_WIDTH = 300;
