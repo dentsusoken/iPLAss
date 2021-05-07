89f7fcdfeb225036f0d13e3a27fa1f0a76c96026
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2021-05-07 16:34:47 +0900

    タブになってしまったのでブランクに変更

--- iplass-admin/src/main/java/gwtupload/client/BaseUploadStatus.java
+++ iplass-admin/src/main/java/gwtupload/client/BaseUploadStatus.java
@@ -200,9 +200,9 @@ public class BaseUploadStatus implements IUploadStatus {
    */
   public void setFileNames(List<String> names) {
     fileNames = names;
-  	//modified by the iPLAss team
+    //modified by the iPLAss team
     //For XSS protection, use GWT's SafeHtml to sanitize upload filenames.
-  	//fileNameLabel.setHTML(Utils.convertCollectionToString(names, "<br/>"));
+    //fileNameLabel.setHTML(Utils.convertCollectionToString(names, "<br/>"));
     fileNameLabel.setHTML(SafeHtmlUtils.fromString(Utils.convertCollectionToString(names, "<br/>")).asString());
     if (prg instanceof HasText) {
       ((HasText) prg).setText(Utils.convertCollectionToString(names, ","));
