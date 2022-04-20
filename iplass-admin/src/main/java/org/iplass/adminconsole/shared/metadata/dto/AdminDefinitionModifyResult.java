469782e0a6b74bcf32459e9d89bb453404b2e03f
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2022-04-20 11:54:28 +0900

    Entity設定のリファクタリング (#866)

--- iplass-admin/src/main/java/org/iplass/adminconsole/shared/metadata/dto/AdminDefinitionModifyResult.java
+++ iplass-admin/src/main/java/org/iplass/adminconsole/shared/metadata/dto/AdminDefinitionModifyResult.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2011 INFORMATION SERVICES INTERNATIONAL - DENTSU, LTD. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -21,6 +21,8 @@
 package org.iplass.adminconsole.shared.metadata.dto;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link org.iplass.mtp.definition.DefinitionModifyResult} のラッパークラス
@@ -36,10 +38,13 @@ public class AdminDefinitionModifyResult implements Serializable {
 	private static final long serialVersionUID = 5017185294034459446L;
 
 	/** 結果 */
-	protected boolean isSuccess;
+	private boolean isSuccess;
 
 	/** メッセージ */
-	protected String message;
+	private String message;
+
+	/** 補足情報 */
+	private Map<String, String> attributes;
 
 	/**
 	 * コンストラクタ
@@ -66,6 +71,7 @@ public class AdminDefinitionModifyResult implements Serializable {
 	public AdminDefinitionModifyResult(boolean isSuccess, String message) {
 		this.isSuccess = isSuccess;
 		this.message = message;
+		this.attributes = new HashMap<>();
 	}
 
 	/**
@@ -85,4 +91,25 @@ public class AdminDefinitionModifyResult implements Serializable {
 	public String getMessage() {
 		return message;
 	}
+
+	/**
+	 * 属性を設定します。
+	 *
+	 * @param key 属性
+	 * @param value 値
+	 */
+	public void setAttribute(String key, String value) {
+		attributes.put(key, value);
+	}
+
+	/**
+	 * 属性を返します。
+	 *
+	 * @param key 属性
+	 * @return 値
+	 */
+	public String getAttribute(String key) {
+		return attributes.get(key);
+	}
+
 }
