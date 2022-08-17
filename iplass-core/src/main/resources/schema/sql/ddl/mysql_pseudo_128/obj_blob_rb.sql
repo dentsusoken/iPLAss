f1d97014fbcc29f25c52972c3370b883d8cba789
xsuzukiyosuke <73982071+xsuzukiyosuke@users.noreply.github.com>
2022-08-17 14:33:42 +0900

    mysql（aurora）のobj_blob_rbテーブルのindex（pk）の変更
    close #938

--- iplass-core/src/main/resources/schema/sql/ddl/mysql_pseudo_128/obj_blob_rb.sql
+++ iplass-core/src/main/resources/schema/sql/ddl/mysql_pseudo_128/obj_blob_rb.sql
@@ -2,8 +2,9 @@ DROP TABLE IF EXISTS `obj_blob_rb`;
 CREATE TABLE `obj_blob_rb` (
   `tenant_id` INT(7) NOT NULL,
   `rb_id` BIGINT(16) NOT NULL,
-  `lob_id` BIGINT(16) NULL,
-  PRIMARY KEY (`tenant_id`, `rb_id`)
+  `lob_id` BIGINT(16) NOT NULL,
+  PRIMARY KEY (`tenant_id`, `lob_id`),
+  INDEX `obj_blob_rb_index1` (`tenant_id`, `rb_id`)
 )
 
 PARTITION BY RANGE (`tenant_id`)
