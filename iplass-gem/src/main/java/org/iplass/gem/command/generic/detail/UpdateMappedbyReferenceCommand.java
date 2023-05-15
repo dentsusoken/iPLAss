bee63937a1dec1314279847106e331f16f08dafe
Yukio Kazama <xkazama.yukio3@contract.isid.co.jp>
2023-05-15 19:11:30 +0900

    close #1179

--- iplass-gem/src/main/java/org/iplass/gem/command/generic/detail/UpdateMappedbyReferenceCommand.java
+++ iplass-gem/src/main/java/org/iplass/gem/command/generic/detail/UpdateMappedbyReferenceCommand.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2012 INFORMATION SERVICES INTERNATIONAL - DENTSU, LTD. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,7 @@ import java.util.List;
 
 import org.iplass.gem.command.CommandUtil;
 import org.iplass.gem.command.Constants;
+import org.iplass.mtp.ApplicationException;
 import org.iplass.mtp.ManagerLocator;
 import org.iplass.mtp.command.Command;
 import org.iplass.mtp.command.RequestContext;
@@ -47,8 +48,8 @@ import org.iplass.mtp.impl.web.token.TokenStore;
 import org.iplass.mtp.util.StringUtil;
 import org.iplass.mtp.web.template.TemplateUtil;
 import org.iplass.mtp.web.template.TemplateUtil.TokenOutputType;
-import org.iplass.mtp.webapi.definition.RequestType;
 import org.iplass.mtp.webapi.definition.MethodType;
+import org.iplass.mtp.webapi.definition.RequestType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,11 +81,19 @@ public final class UpdateMappedbyReferenceCommand implements Command {
 
 	@Override
 	public String execute(RequestContext request) {
+
+		// 参照元Entity定義名
 		String defName = request.getParam(Constants.DEF_NAME);
+		// 参照元OID
 		String oid = request.getParam(Constants.OID);
+		// 更新対象プロパティ（被参照プロパティ）
 		String updatePropertyName = request.getParam("updatePropertyName");
+		// 削除対象件数
 		String delCountStr = request.getParam("delCount");
+		// 物理削除（親子関係の場合）
+		// FIXME DetailView定義から取得する
 		boolean purge = Boolean.parseBoolean(request.getParam("purge"));
+
 		int delCount = 0;
 		try {
 			delCount = Integer.parseInt(delCountStr);
@@ -95,16 +104,29 @@ public final class UpdateMappedbyReferenceCommand implements Command {
 		PropertyDefinition pd = ed.getProperty(updatePropertyName);
 		if (pd instanceof ReferenceProperty) {
 			ReferenceProperty rp = (ReferenceProperty) pd;
-			EntityDefinition red = edm.get(rp.getObjectDefinitionName());
-			if (rp.getMappedBy() != null && !rp.getMappedBy().isEmpty()) {
-				//被参照の削除の場合は相手を更新
+			if (StringUtil.isNotEmpty(rp.getMappedBy())) {
+				EntityDefinition red = edm.get(rp.getObjectDefinitionName());
 				for (int i = 0; i < delCount; i++) {
+					// 削除対象のoid、version取得
 					String refEntityKey = request.getParam("refEntityKey_" + i);
+
+					if (StringUtil.isBlank(refEntityKey)) {
+						continue;
+					}
+
+					Entity refEntity = loadReference(refEntityKey, rp);
+					if (refEntity == null) {
+						// 既に削除されているのでスキップ
+						continue;
+					}
+
 					try {
 						if (rp.getReferenceType() == ReferenceType.ASSOCIATION) {
-							update(oid, refEntityKey, rp, red);
+							// 被参照の削除の場合は相手を更新
+							update(oid, refEntity, rp, red);
 						} else if (rp.getReferenceType() == ReferenceType.COMPOSITION) {
-							delete(refEntityKey, rp, purge);
+							// 親子の場合は削除
+							delete(refEntity, purge);
 						}
 					} catch (EntityValidationException e) {
 						if (logger.isDebugEnabled()) {
@@ -112,6 +134,14 @@ public final class UpdateMappedbyReferenceCommand implements Command {
 						}
 						List<ValidateError> errors = new ArrayList<ValidateError>();
 						errors.addAll(e.getValidateResults());
+						request.setAttribute(Constants.ERROR_PROP, errors);
+						return Constants.CMD_EXEC_ERROR;
+					} catch (ApplicationException e) {
+						if (logger.isDebugEnabled()) {
+							logger.debug(e.getMessage(), e);
+						}
+						List<String> errors = new ArrayList<>();
+						errors.add(e.getMessage());
 						request.setAttribute(Constants.ERROR_PROP, errors);
 						return Constants.CMD_EXEC_ERROR;
 					}
@@ -126,17 +156,13 @@ public final class UpdateMappedbyReferenceCommand implements Command {
 		return Constants.CMD_EXEC_SUCCESS;
 	}
 
-	private void update(String oid, String refEntityKey, ReferenceProperty rp, EntityDefinition red) {
-		if (StringUtil.isBlank(refEntityKey)) return;
-
-		int lastIndex = refEntityKey.lastIndexOf("_");
-		String roid = refEntityKey.substring(0, lastIndex);
-		Long rversion = CommandUtil.getLong(refEntityKey.substring(lastIndex + 1));
-		Entity refEntity = em.load(roid, rversion, rp.getObjectDefinitionName());
+	private void update(String oid, Entity refEntity, ReferenceProperty rp, EntityDefinition red) {
+		boolean needUpdate = false;
 		ReferenceProperty rrp = (ReferenceProperty) red.getProperty(rp.getMappedBy());
 		if (rrp.getMultiplicity() == 1) {
 			Entity entity = refEntity.getValue(rrp.getName());
 			if (entity != null && entity.getOid().equals(oid)) {
+				needUpdate = true;
 				refEntity.setValue(rrp.getName(), null);
 			}
 		} else {
@@ -144,29 +170,45 @@ public final class UpdateMappedbyReferenceCommand implements Command {
 			if (array != null) {
 				List<Entity> entityList = new ArrayList<Entity>();
 				for (Entity entity : array) {
-					if (entity != null && !entity.getOid().equals(oid)) {
+					if (entity.getOid().equals(oid)) {
+						// 除外
+						needUpdate = true;
+					} else {
 						entityList.add(entity);
 					}
 				}
 				refEntity.setValue(rrp.getName(), entityList.toArray(new Entity[entityList.size()]));
 			}
 		}
-		UpdateOption option = new UpdateOption(true);
-		option.setUpdateProperties(rrp.getName());
-		em.update(refEntity, option);
-	}
-	private void delete(String refEntityKey, ReferenceProperty rp, boolean purge) {
-		if (StringUtil.isBlank(refEntityKey)) return;
-
-		int lastIndex = refEntityKey.lastIndexOf("_");
-		String roid = refEntityKey.substring(0, lastIndex);
-		Long rversion = CommandUtil.getLong(refEntityKey.substring(lastIndex + 1));
-		Entity refEntity = em.load(roid,  rversion, rp.getObjectDefinitionName());
-		if (refEntity != null) {
-			DeleteOption option = new DeleteOption();
-			option.setCheckTimestamp(false);
-			option.setPurge(purge);
-			em.delete(refEntity, option);
+		if (needUpdate) {
+			UpdateOption option = new UpdateOption(true);
+			option.setUpdateProperties(rrp.getName());
+			em.update(refEntity, option);
+		} else {
+			logger.info("mapped by reference delete target data has been updated. reference entity="
+					+ rp.getObjectDefinitionName() + ", property=" + rrp.getName()
+					+ ", oid=" + refEntity.getOid() + ", version=" + refEntity.getVersion()
+					+ ", reference oid=" + oid);
 		}
 	}
+
+	private void delete(Entity refEntity, boolean purge) {
+		DeleteOption option = new DeleteOption();
+		option.setCheckTimestamp(false);
+		option.setPurge(purge);
+		em.delete(refEntity, option);
+	}
+
+	private Entity loadReference(String refEntityKey, ReferenceProperty rp) {
+		int lastIndex = refEntityKey.lastIndexOf("_");
+		String oid = refEntityKey.substring(0, lastIndex);
+		Long version = CommandUtil.getLong(refEntityKey.substring(lastIndex + 1));
+		Entity refEntity = em.load(oid, version, rp.getObjectDefinitionName());
+		if (refEntity == null) {
+			logger.info("mapped by reference delete target data has been deleted. reference entity="
+					+ rp.getObjectDefinitionName() + ", oid=" + oid + ", version=" + version);
+		}
+		return refEntity;
+	}
+
 }
