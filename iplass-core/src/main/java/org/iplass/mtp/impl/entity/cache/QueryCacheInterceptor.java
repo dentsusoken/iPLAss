cffdc4b8b7c63f7c30b545d713c659ad01d9f6ae
HiguchiKiyoshi <higuchi.kiyoshi@isid.co.jp>
2023-06-30 14:26:17 +0900

    close #1223

--- iplass-core/src/main/java/org/iplass/mtp/impl/entity/cache/QueryCacheInterceptor.java
+++ iplass-core/src/main/java/org/iplass/mtp/impl/entity/cache/QueryCacheInterceptor.java
@@ -49,7 +49,9 @@ import org.iplass.mtp.impl.cache.store.CacheStore;
 import org.iplass.mtp.impl.core.ExecuteContext;
 import org.iplass.mtp.impl.entity.EntityContext;
 import org.iplass.mtp.impl.entity.EntityHandler;
+import org.iplass.mtp.impl.entity.interceptor.EntityCountInvocationImpl;
 import org.iplass.mtp.impl.entity.interceptor.EntityInvocationImpl;
+import org.iplass.mtp.impl.entity.interceptor.EntityQueryInvocationImpl;
 import org.iplass.mtp.spi.ServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,31 +66,35 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 		return cacheService.getCache(EntityCacheInterceptor.QUERY_CACHE_NAMESPACE_PREFIX + ExecuteContext.getCurrentContext().getClientTenantId(), withCreate);
 	}
 
-	private void notifyUpdate(String defName) {
-		CacheStore cache = getCache(false);
-		if (cache != null
-				&& isEnableCache(defName, EntityContext.getCurrentContext())) {
-			cache.removeByIndex(0, defName);
+	private void notifyUpdate(EntityInvocation<?> invocation) {
+		EntityHandler eh = ((EntityInvocationImpl<?>) invocation).getEntityHandler();
+		if (eh.getMetaData().isQueryCache()) {
+			CacheStore cache = getCache(false);
+			if (cache != null) {
+				synchronized(eh) {
+					cache.removeByIndex(0, eh.getMetaData().getName());
+				}
+			}
 		}
 	}
 
 	@Override
 	public String insert(EntityInsertInvocation invocation) {
 		String ret = invocation.proceed();
-		notifyUpdate(invocation.getEntity().getDefinitionName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 
 	@Override
 	public void update(EntityUpdateInvocation invocation) {
 		invocation.proceed();
-		notifyUpdate(invocation.getEntity().getDefinitionName());
+		notifyUpdate(invocation);
 	}
 
 	@Override
 	public void delete(EntityDeleteInvocation invocation) {
 		invocation.proceed();
-		notifyUpdate(invocation.getEntity().getDefinitionName());
+		notifyUpdate(invocation);
 	}
 
 	private CacheHint hasCacheHintAndEnableCache(Query q, EntityInvocation<?> inv) {
@@ -111,20 +117,16 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 		return null;
 	}
 
-	private boolean isEnableCache(String defName, EntityContext ec) {
-		EntityHandler eh = ec.getHandlerByName(defName);
-		if (eh == null) {
-			return false;
-		}
-		return eh.getMetaData().isQueryCache();
-	}
-
 	private boolean isEnableCache(String[] defNameList, EntityContext ec) {
 		if (defNameList == null || defNameList.length == 0) {
 			return false;
 		}
 		for (String defName: defNameList) {
-			if (!isEnableCache(defName, ec)) {
+			EntityHandler eh = ec.getHandlerByName(defName);
+			if (eh == null) {
+				return false;
+			}
+			if (!eh.getMetaData().isQueryCache()) {
 				return false;
 			}
 		}
@@ -160,22 +162,29 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 				Predicate<?> callback = invocation.getPredicate();
 				CacheStore store = getCache(true);
 				CacheEntry ce = store.get(new QueryCacheKey(q, invocation.getSearchOption().isReturnStructuredEntity()));
-				if (ce == null
-						|| ce.getValue() == null
-						|| !((QueryCache) ce.getValue()).canIterate(invocation.getType())
-						|| ((QueryCache) ce.getValue()).eol()) {
-					ArrayList<Object> list = new ArrayList<>();
-					invocation.setPredicate(dataModel -> {
-						list.add(dataModel);
-						return true;
-					});
+				if (isInvalidQueryCache(invocation, ce)) {
+					//同一クエリーの検索がバックエンドに発行されないように。（結局はEntity定義単位でロックが必要となるのでEntityHandlerでロック）
+					synchronized(((EntityQueryInvocationImpl) invocation).getEntityHandler()) {
+						ce = store.get(new QueryCacheKey(q, invocation.getSearchOption().isReturnStructuredEntity()));
+						if (isInvalidQueryCache(invocation, ce)) {
+							ArrayList<Object> list = new ArrayList<>();
+							invocation.setPredicate(dataModel -> {
+								list.add(dataModel);
+								return true;
+							});
 
-					invocation.proceed();
+							invocation.proceed();
 
-					QueryCache qc = new QueryCache(list.size(), list, invocation.getType(), hint.getTTL());
-					q = q.copy();
-					ce = new CacheEntry(new QueryCacheKey(q, invocation.getSearchOption().isReturnStructuredEntity()), qc, (Object) usedEntityDefs);
-					store.put(ce, true);
+							QueryCache qc = new QueryCache(list.size(), list, invocation.getType(), hint.getTTL());
+							q = q.copy();
+							ce = new CacheEntry(new QueryCacheKey(q, invocation.getSearchOption().isReturnStructuredEntity()), qc, (Object) usedEntityDefs);
+							store.put(ce, true);
+						} else {
+							if (logger.isTraceEnabled()) {
+								logger.trace("Result list from global cache:" + q);
+							}
+						}
+					}
 				} else {
 					if (logger.isTraceEnabled()) {
 						logger.trace("Result list from global cache:" + q);
@@ -188,6 +197,20 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 				invocation.proceed();
 			}
 		}
+	}
+
+	private boolean isInvalidQueryCache(EntityQueryInvocation invocation, CacheEntry ce) {
+		return ce == null
+				|| ce.getValue() == null
+				|| !((QueryCache) ce.getValue()).canIterate(invocation.getType())
+				|| ((QueryCache) ce.getValue()).eol();
+	}
+
+	private boolean isInvalidCountCache(CacheEntry ce) {
+		return ce == null
+				|| ce.getValue() == null
+				|| ((QueryCache) ce.getValue()).getCount() == null
+				|| ((QueryCache) ce.getValue()).eol();
 	}
 
 	@Override
@@ -209,14 +232,22 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 		if (hint != null && usedEntityDefs != null) {
 			CacheStore store = getCache(true);
 			CacheEntry ce = store.get(new QueryCacheKey(q, false));
-			if (ce == null
-					|| ce.getValue() == null
-					|| ((QueryCache) ce.getValue()).getCount() == null) {
-				int ret = invocation.proceed();
-				QueryCache qc = new QueryCache(ret, null, invocation.getType(), hint.getTTL());
-				q = q.copy();
-				ce = new CacheEntry(new QueryCacheKey(q, false), qc, (Object) usedEntityDefs);
-				store.put(ce, true);
+			if (isInvalidCountCache(ce)) {
+				//同一クエリーの検索がバックエンドに発行されないように。（結局はEntity定義単位でロックが必要となるのでEntityHandlerでロック）
+				synchronized(((EntityCountInvocationImpl) invocation).getEntityHandler()) {
+					ce = store.get(new QueryCacheKey(q, false));
+					if (isInvalidCountCache(ce)) {
+						int ret = invocation.proceed();
+						QueryCache qc = new QueryCache(ret, null, invocation.getType(), hint.getTTL());
+						q = q.copy();
+						ce = new CacheEntry(new QueryCacheKey(q, false), qc, (Object) usedEntityDefs);
+						store.put(ce, true);
+					} else {
+						if (logger.isTraceEnabled()) {
+							logger.trace("Result list from global cache:" + q);
+						}
+					}
+				}
 			} else {
 				if (logger.isTraceEnabled()) {
 					logger.trace("Result list from global cache:" + q);
@@ -230,23 +261,24 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 		}
 	}
 
+
 	@Override
 	public int updateAll(EntityUpdateAllInvocation invocation) {
 		int ret = invocation.proceed();
-		notifyUpdate(invocation.getUpdateCondition().getDefinitionName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 
 	@Override
 	public void bulkUpdate(EntityBulkUpdateInvocation invocation) {
 		invocation.proceed();
-		notifyUpdate(invocation.getBulkUpdatable().getDefinitionName());
+		notifyUpdate(invocation);
 	}
 
 	@Override
 	public int deleteAll(EntityDeleteAllInvocation invocation) {
 		int ret = invocation.proceed();
-		notifyUpdate(invocation.getDeleteCondition().getDefinitionName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 
@@ -258,21 +290,21 @@ class QueryCacheInterceptor extends EntityInterceptorAdapter {
 	@Override
 	public Entity restore(EntityRestoreInvocation invocation) {
 		Entity ret = invocation.proceed();
-		notifyUpdate(invocation.getEntityDefinition().getName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 
 	@Override
 	public boolean lockByUser(EntityLockByUserInvocation invocation) {
 		boolean ret = invocation.proceed();
-		notifyUpdate(invocation.getEntityDefinition().getName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 
 	@Override
 	public boolean unlockByUser(EntityUnlockByUserInvocation invocation) {
 		boolean ret = invocation.proceed();
-		notifyUpdate(invocation.getEntityDefinition().getName());
+		notifyUpdate(invocation);
 		return ret;
 	}
 }
