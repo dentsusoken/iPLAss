5e9c77f59cf83efd0cb3a49e0fbc4f306db842f7
xsekiguchi <xsekiguchi.naoya2@contract.isid.co.jp>
2023-10-12 10:45:30 +0900

    CKEditorバージョンアップ
    - ライブラリバージョンアップ（4.14.0 -> 4.22.1）
    - exportpdf プラグイン削除（Error code: exportpdf-no-token-url の抑制）

--- iplass-gem/src/main/resources/META-INF/resources/scripts/gem/plugin/ckeditor/mtpconfig.js
+++ iplass-gem/src/main/resources/META-INF/resources/scripts/gem/plugin/ckeditor/mtpconfig.js
@@ -35,7 +35,7 @@ CKEDITOR.editorConfig = function( config )
 	CKEDITOR.config.font_names='ＭＳ Ｐゴシック;ＭＳ Ｐ明朝;ＭＳ ゴシック;ＭＳ 明朝;Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif';
 	CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
 	CKEDITOR.config.toolbarCanCollapse = false;
-	CKEDITOR.config.removePlugins = 'maximize,resize';
+	CKEDITOR.config.removePlugins = 'maximize,resize,exportpdf';
 
 	CKEDITOR.dtd['a']['article'] = 1;
 	CKEDITOR.dtd['a']['aside'] = 1;
