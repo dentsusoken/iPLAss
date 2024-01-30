abd8e9ef1b9ccf2471b006a07a634b3d6be345d8
yoshiharuyasuda <xyasuda.yoshiharu@contract.dentsusoken.com>
2024-01-30 16:24:20 +0900

    close #1385

--- iplass-web/src/main/java/org/iplass/mtp/impl/webapi/EntityWebApiService.java
+++ iplass-web/src/main/java/org/iplass/mtp/impl/webapi/EntityWebApiService.java
@@ -20,7 +20,10 @@
 
 package org.iplass.mtp.impl.webapi;
 
+import java.util.List;
+
 import org.iplass.mtp.ManagerLocator;
+import org.iplass.mtp.auth.AuthContext;
 import org.iplass.mtp.definition.TypedDefinitionManager;
 import org.iplass.mtp.impl.definition.AbstractTypedMetaDataService;
 import org.iplass.mtp.impl.definition.DefinitionMetaDataTypeMap;
@@ -71,6 +74,9 @@ public class EntityWebApiService extends AbstractTypedMetaDataService<MetaEntity
 
 	private boolean throwSearchResultLimitExceededException;
 
+	/** EntityCRUD API 制御オプション許可ロール */
+	private List<String> permitRolesToSpecifyOptions;
+
 	public boolean isEnableNativeHint() {
 		return enableNativeHint;
 	}
@@ -108,6 +114,28 @@ public class EntityWebApiService extends AbstractTypedMetaDataService<MetaEntity
 		return csvTimeFormat;
 	}
 
+	/**
+	 * EntityCRUD APIで制御オプションが許可されたロールか
+	 * @return 許可されたロールの場合true
+	 */
+	public boolean isPermitRolesToSpecifyOptions() {
+		AuthContext auth = AuthContext.getCurrentContext();
+		if (auth.getUser().isAdmin()) {
+			// adminなら許可
+			return true;
+		}
+
+		if (permitRolesToSpecifyOptions != null && !permitRolesToSpecifyOptions.isEmpty()) {
+			for (String role : permitRolesToSpecifyOptions) {
+				if (auth.userInRole(role)) {
+					//指定ロールなら許可
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public void destroy() {
 	}
@@ -133,6 +161,8 @@ public class EntityWebApiService extends AbstractTypedMetaDataService<MetaEntity
 		loadWithMappedByReference = config.getValue("loadWithMappedByReference", Boolean.TYPE, Boolean.FALSE);
 
 		throwSearchResultLimitExceededException = config.getValue("throwSearchResultLimitExceededException", Boolean.TYPE, Boolean.FALSE);
+
+		permitRolesToSpecifyOptions = config.getValues("permitRolesToSpecifyOptions");
 	}
 
 	public static String getFixedPath() {
--- iplass-web/src/main/java/org/iplass/mtp/impl/webapi/command/entity/CreateEntityCommand.java
+++ iplass-web/src/main/java/org/iplass/mtp/impl/webapi/command/entity/CreateEntityCommand.java
@@ -21,6 +21,8 @@ package org.iplass.mtp.impl.webapi.command.entity;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import org.iplass.mtp.SystemException;
 import org.iplass.mtp.async.TaskStatus;
@@ -31,12 +33,20 @@ import org.iplass.mtp.command.annotation.webapi.RestJson;
 import org.iplass.mtp.command.annotation.webapi.RestXml;
 import org.iplass.mtp.command.annotation.webapi.WebApi;
 import org.iplass.mtp.entity.Entity;
+import org.iplass.mtp.entity.InsertOption;
 import org.iplass.mtp.impl.csv.CsvUploadService;
 import org.iplass.mtp.impl.csv.CsvUploadStatus;
+import org.iplass.mtp.impl.csv.EntityCsvImportOption;
+import org.iplass.mtp.impl.csv.EntityCsvImportResult;
+import org.iplass.mtp.impl.csv.EntityCsvImportService;
 import org.iplass.mtp.impl.csv.TransactionType;
+import org.iplass.mtp.impl.entity.EntityService;
 import org.iplass.mtp.impl.entity.csv.EntityCsvException;
+import org.iplass.mtp.impl.metadata.MetaDataContext;
+import org.iplass.mtp.impl.metadata.MetaDataEntry;
 import org.iplass.mtp.impl.web.WebRequestContext;
 import org.iplass.mtp.impl.web.fileupload.MultiPartParameterValueMap;
+import org.iplass.mtp.impl.webapi.EntityWebApiService;
 import org.iplass.mtp.spi.ServiceRegistry;
 import org.iplass.mtp.util.StringUtil;
 import org.iplass.mtp.webapi.definition.MethodType;
@@ -48,7 +58,8 @@ import org.iplass.mtp.webapi.definition.RequestType;
 		restJson=@RestJson(parameterName=CreateEntityCommand.PARAM_ENTITY, parameterType=Entity.class),
 		restXml=@RestXml(parameterName=CreateEntityCommand.PARAM_ENTITY),
 		results={CreateEntityCommand.RESULT_OID,
-				CreateEntityCommand.RESULT_UPLOAD_INSERT, CreateEntityCommand.RESULT_UPLOAD_UPDATE, CreateEntityCommand.RESULT_UPLOAD_DELETE},
+				CreateEntityCommand.RESULT_UPLOAD_INSERT, CreateEntityCommand.RESULT_UPLOAD_UPDATE,
+				CreateEntityCommand.RESULT_UPLOAD_DELETE, CreateEntityCommand.RESULT_UPLOAD_ERROR},
 		supportBearerToken = true,
 		overwritable=false)
 @CommandClass(name="mtp/entity/CreateEntityCommand", displayName="Entity Create Web API", overwritable=false)
@@ -57,13 +68,37 @@ public final class CreateEntityCommand extends AbstractEntityCommand {
 	public static final String PARAM_ENTITY = "entity";
 	public static final String RESULT_OID = "oid";
 
+	// POSTのオプションパラメータ
+	public static final String PARAM_POST_WITH_VALIDATION = "withValidation";
+	public static final String PARAM_POST_NOTIFY_LISTENERS = "notifyListeners";
+	public static final String PARAM_POST_ENABLE_AUDIT_PROPERTY_SPECIFICATION = "enableAuditPropertySpecification";
+	public static final String PARAM_POST_REGENERATE_OID = "regenerateOid";
+	public static final String PARAM_POST_REGENERATE_AUTO_NUMBER = "regenerateAutoNumber";
+	public static final String PARAM_POST_VERSION_SPECIFIED = "versionSpecified";
+	public static final String PARAM_POST_LOCALIZED = "localized";
+
+	// POST(Upload)のオプションパラメータ
 	public static final String PARAM_UPLOAD_FILE = "uploadFile";
 	public static final String PARAM_UPLOAD_UNIQUE_KEY = "uniqueKey";
 	public static final String PARAM_UPLOAD_ASYNC = "asyncUpload";
+	public static final String PARAM_UPLOAD_TRUNCATE = "truncate";
+	public static final String PARAM_UPLOAD_BULK_UPDATE = "bulkUpdate";
+	public static final String PARAM_UPLOAD_ERROR_SKIP = "errorSkip";
+	public static final String PARAM_UPLOAD_IGNORE_NOT_EXIST_PROPERTY = "ignoreNotExistsProperty";
+	public static final String PARAM_UPLOAD_NOTIFY_LISTENERT = "notifyListeners";
+	public static final String PARAM_UPLOAD_WITH_VALIDATION = "withValidation";
+	public static final String PARAM_UPLOAD_UPDATE_DISUPDATABLE_PROPERTY = "updateDisupdatableProperty";
+	public static final String PARAM_UPLOAD_INSERT_ENABLE_AUDIT_PROPERTY_SPECIFICATION = "insertEnableAuditPropertySpecification";
+	public static final String PARAM_UPLOAD_PREFIX_OID = "prefixOid";
+	public static final String PARAM_UPLOAD_COMMIT_LIMIT = "commitLimit";
+	public static final String PARAM_UPLOAD_FORCE_UPDATE = "fourceUpdate";
+	public static final String PARAM_UPLOAD_LOCALE = "locale";
+	public static final String PARAM_UPLOAD_TIMEZONE = "timezone";
 
 	public static final String RESULT_UPLOAD_INSERT = "insert";
 	public static final String RESULT_UPLOAD_UPDATE = "update";
 	public static final String RESULT_UPLOAD_DELETE = "delete";
+	public static final String RESULT_UPLOAD_ERROR = "error";
 
 	//entity/[defName]/
 	//entity/[defName]/ multipart 一括アップロード（csv）
@@ -95,8 +130,52 @@ public final class CreateEntityCommand extends AbstractEntityCommand {
 		Entity e = (Entity) request.getAttribute(PARAM_ENTITY);
 		e.setDefinitionName(defName);
 
-		String oid = em.insert(e);
+		InsertOption option = new InsertOption();
+		setInsertOptionWithParam(request, option);
+
+		String oid = em.insert(e, option);
 		request.setAttribute(RESULT_OID, oid);
+	}
+
+	/**
+	 * リクエストパラメータに設定されたオプションをInsertOptionに設定
+	 *
+	 * @param request リクエスト
+	 * @param option InsertOption
+	 */
+	private void setInsertOptionWithParam(RequestContext request, InsertOption option) {
+		EntityWebApiService service = ServiceRegistry.getRegistry().getService(EntityWebApiService.class);
+
+		if (service.isPermitRolesToSpecifyOptions()) {
+			String withValidation = request.getParam(PARAM_POST_WITH_VALIDATION);
+			if (StringUtil.isNotBlank(withValidation)) {
+				option.setWithValidation(Boolean.parseBoolean(withValidation));
+			}
+			String notifyListeners = request.getParam(PARAM_POST_NOTIFY_LISTENERS);
+			if (StringUtil.isNotBlank(notifyListeners)) {
+				option.setNotifyListeners(Boolean.parseBoolean(notifyListeners));
+			}
+			String enableAuditPropertySpecification = request.getParam(PARAM_POST_ENABLE_AUDIT_PROPERTY_SPECIFICATION);
+			if (StringUtil.isNotBlank(enableAuditPropertySpecification)) {
+				option.setEnableAuditPropertySpecification(Boolean.parseBoolean(enableAuditPropertySpecification));
+			}
+			String regenerateOid = request.getParam(PARAM_POST_REGENERATE_OID);
+			if (StringUtil.isNotBlank(regenerateOid)) {
+				option.setRegenerateOid(Boolean.parseBoolean(regenerateOid));
+			}
+			String regenerateAutoNumber = request.getParam(PARAM_POST_REGENERATE_AUTO_NUMBER);
+			if (StringUtil.isNotBlank(regenerateAutoNumber)) {
+				option.setRegenerateAutoNumber(Boolean.parseBoolean(regenerateAutoNumber));
+			}
+			String versionSpecified = request.getParam(PARAM_POST_VERSION_SPECIFIED);
+			if (StringUtil.isNotBlank(versionSpecified)) {
+				option.setVersionSpecified(Boolean.parseBoolean(versionSpecified));
+			}
+			String localized = request.getParam(PARAM_POST_LOCALIZED);
+			if (StringUtil.isNotBlank(localized)) {
+				option.setLocalized(Boolean.parseBoolean(localized));
+			}
+		}
 	}
 
 	private void doCsvUpload(RequestContext request, String defName) {
@@ -112,37 +191,153 @@ public final class CreateEntityCommand extends AbstractEntityCommand {
 		}
 
 		CsvUploadService service = ServiceRegistry.getRegistry().getService(CsvUploadService.class);
+		EntityCsvImportService importService = ServiceRegistry.getRegistry().getService(EntityCsvImportService.class);
+		EntityCsvImportOption option = new EntityCsvImportOption();
+		setImportOptionWithParam(request, option);
 
 		if (async) {
-			// 非同期アップロード(トランザクション分割なし)
-			try (InputStream is = file.getInputStream()) {
-				service.asyncUpload(is, file.getFileName(), defName, null, uniqueKey, false, false, false, null, null, TransactionType.ONCE, 0, true, true);
-			} catch (IOException e) {
-				throw new SystemException(e);
+			if (option.isWithOption()) {
+				//管理者用オプションあり
+				try (InputStream is = file.getInputStream()) {
+					List<String> excludeEntityNames = Arrays.asList("mtp.maintenance.Package", "mtp.maintenance.MetaDataTag");
+					importService.asyncImportEntityData(defName, is, defName, option, excludeEntityNames);
+				} catch (IOException e) {
+					throw new SystemException(e);
+				}
+			} else {
+				// 非同期アップロード(トランザクション分割なし)
+				try (InputStream is = file.getInputStream()) {
+					service.asyncUpload(is, file.getFileName(), defName, null, uniqueKey, false, false, false, null, null, TransactionType.ONCE, 0, true, true);
+				} catch (IOException e) {
+					throw new SystemException(e);
+				}
 			}
 		} else {
-			// ファイルの整合性チェック
-			try (InputStream is = file.getInputStream()) {
-				service.validate(is, defName, true);
-			} catch (IOException e) {
-				throw new SystemException(e);
-			}
+			if (option.isWithOption()) {
+				//管理者用オプションあり
+				//MetaDataEntry
+				String entityPath = EntityService.ENTITY_META_PATH + defName.replace(".", "/");
+				MetaDataEntry entry = MetaDataContext.getContext().getMetaDataEntry(entityPath);
+				List<String> excludeEntityNames = Arrays.asList("mtp.maintenance.Package", "mtp.maintenance.MetaDataTag");
+				try (InputStream is = file.getInputStream()) {
+					EntityCsvImportResult ret = importService.importEntityData(defName, is, entry, option, null, null, excludeEntityNames);
 
-			// 同期アップロード(トランザクション分割なし)
-			try (InputStream is = file.getInputStream()) {
-				CsvUploadStatus result = service.upload(is, defName, uniqueKey, false, false, false, null, null, TransactionType.ONCE, 0, true, true);
-				if (result.getStatus() != TaskStatus.COMPLETED) {
-					throw new EntityCsvException(result.getCode(), result.getMessage());
+					request.setAttribute(RESULT_UPLOAD_INSERT, ret.getInsertCount());
+					request.setAttribute(RESULT_UPLOAD_UPDATE, ret.getUpdateCount());
+					request.setAttribute(RESULT_UPLOAD_DELETE, ret.getDeleteCount());
+					request.setAttribute(RESULT_UPLOAD_ERROR, ret.getErrorCount());
+				} catch (IOException e) {
+					throw new SystemException(e);
+				}
+			} else {
+				// ファイルの整合性チェック
+				try (InputStream is = file.getInputStream()) {
+					service.validate(is, defName, true);
+				} catch (IOException e) {
+					throw new SystemException(e);
 				}
 
-				request.setAttribute(RESULT_UPLOAD_INSERT, result.getInsertCount());
-				request.setAttribute(RESULT_UPLOAD_UPDATE, result.getUpdateCount());
-				request.setAttribute(RESULT_UPLOAD_DELETE, result.getDeleteCount());
-			} catch (IOException e) {
-				throw new SystemException(e);
-			}
+				// 同期アップロード(トランザクション分割なし)
+				try (InputStream is = file.getInputStream()) {
+					CsvUploadStatus result = service.upload(is, defName, uniqueKey, false, false, false, null, null, TransactionType.ONCE, 0, true, true);
+					if (result.getStatus() != TaskStatus.COMPLETED) {
+						throw new EntityCsvException(result.getCode(), result.getMessage());
+					}
 
+					request.setAttribute(RESULT_UPLOAD_INSERT, result.getInsertCount());
+					request.setAttribute(RESULT_UPLOAD_UPDATE, result.getUpdateCount());
+					request.setAttribute(RESULT_UPLOAD_DELETE, result.getDeleteCount());
+				} catch (IOException e) {
+					throw new SystemException(e);
+				}
+			}
 		}
 	}
 
+	/**
+	 * リクエストパラメータに設定されたオプションをEntityCsvImportOptionに設定
+	 *
+	 * @param request リクエスト
+	 * @param option EntityCsvImportOption
+	 */
+	private void setImportOptionWithParam(RequestContext request, EntityCsvImportOption option) {
+		EntityWebApiService service = ServiceRegistry.getRegistry().getService(EntityWebApiService.class);
+
+		if (service.isPermitRolesToSpecifyOptions()) {
+			String truncate = request.getParam(PARAM_UPLOAD_TRUNCATE);
+			if (StringUtil.isNotBlank(truncate)) {
+				option.setTruncate(Boolean.parseBoolean(truncate));
+				option.withOption();
+			}
+			String bulkUpdate = request.getParam(PARAM_UPLOAD_BULK_UPDATE);
+			if (StringUtil.isNotBlank(bulkUpdate)) {
+				option.setBulkUpdate(Boolean.parseBoolean(bulkUpdate));
+				option.withOption();
+			}
+			String errorSkip = request.getParam(PARAM_UPLOAD_ERROR_SKIP);
+			if (StringUtil.isNotBlank(errorSkip)) {
+				option.setErrorSkip(Boolean.parseBoolean(errorSkip));
+				option.withOption();
+			}
+			String ignoreNotExistsProperty = request.getParam(PARAM_UPLOAD_IGNORE_NOT_EXIST_PROPERTY);
+			if (StringUtil.isNotBlank(ignoreNotExistsProperty)) {
+				option.setIgnoreNotExistsProperty(Boolean.parseBoolean(ignoreNotExistsProperty));
+				option.withOption();
+			}
+			String notifyListeners = request.getParam(PARAM_UPLOAD_NOTIFY_LISTENERT);
+			if (StringUtil.isNotBlank(notifyListeners)) {
+				option.setNotifyListeners(Boolean.parseBoolean(notifyListeners));
+				option.withOption();
+			}
+			String withValidation = request.getParam(PARAM_UPLOAD_WITH_VALIDATION);
+			if (StringUtil.isNotBlank(withValidation)) {
+				option.setWithValidation(Boolean.parseBoolean(withValidation));
+				option.withOption();
+			}
+			String updateDisupdatableProperty = request.getParam(PARAM_UPLOAD_UPDATE_DISUPDATABLE_PROPERTY);
+			if (StringUtil.isNotBlank(updateDisupdatableProperty)) {
+				option.setUpdateDisupdatableProperty(Boolean.parseBoolean(updateDisupdatableProperty));
+				option.withOption();
+			}
+			String insertEnableAuditPropertySpecification = request.getParam(PARAM_UPLOAD_INSERT_ENABLE_AUDIT_PROPERTY_SPECIFICATION);
+			if (StringUtil.isNotBlank(insertEnableAuditPropertySpecification)) {
+				option.setInsertEnableAuditPropertySpecification(Boolean.parseBoolean(insertEnableAuditPropertySpecification));
+				option.withOption();
+			}
+			String prefixOid = request.getParam(PARAM_UPLOAD_PREFIX_OID);
+			if (StringUtil.isNotBlank(prefixOid)) {
+				option.setPrefixOid(prefixOid);
+				option.withOption();
+			}
+			String commitLimit = request.getParam(PARAM_UPLOAD_COMMIT_LIMIT);
+			if (StringUtil.isNotBlank(commitLimit)) {
+				try {
+					option.setCommitLimit(Integer.parseInt(commitLimit));
+					option.withOption();
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException("illegal parameter:" + PARAM_UPLOAD_COMMIT_LIMIT);
+				}
+			}
+			String fourceUpdate = request.getParam(PARAM_UPLOAD_FORCE_UPDATE);
+			if (StringUtil.isNotBlank(fourceUpdate)) {
+				option.setFourceUpdate(Boolean.parseBoolean(fourceUpdate));
+				option.withOption();
+			}
+			String uniqueKey = request.getParam(PARAM_UPLOAD_UNIQUE_KEY);
+			if (StringUtil.isNotBlank(uniqueKey)) {
+				option.setUniqueKey(uniqueKey);
+				//option.withOption(); 既存のオプションでも有効なため、他の項目で判定
+			}
+			String locale = request.getParam(PARAM_UPLOAD_LOCALE);
+			if (StringUtil.isNotBlank(locale)) {
+				option.setLocale(locale);
+				option.withOption();
+			}
+			String timezone = request.getParam(PARAM_UPLOAD_TIMEZONE);
+			if (StringUtil.isNotBlank(timezone)) {
+				option.setTimezone(timezone);
+				option.withOption();
+			}
+		}
+	}
 }
