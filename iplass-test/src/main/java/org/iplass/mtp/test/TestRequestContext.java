4d39b272c372c747cdf5423977a0a773f23cd585
kageyama <kageyama.kohei@dentsusoken.com>
2024-02-02 10:15:47 +0900

    再修正＋修正漏れ箇所の修正

--- iplass-test/src/main/java/org/iplass/mtp/test/TestRequestContext.java
+++ iplass-test/src/main/java/org/iplass/mtp/test/TestRequestContext.java
@@ -432,14 +432,14 @@ public class TestRequestContext implements RequestContext {
 			Session s = ss.getSession(create);
 			if (s != null) {
 				int tenantId = ExecuteContext.getCurrentContext().getClientTenantId();
-				session = (SimpleSessionContext) s.getAttribute("com.dentsusoken.mtp.sessionContext." + tenantId);
+				session = (SimpleSessionContext) s.getAttribute("org.iplass.mtp.sessionContext." + tenantId);
 
 				if (session == null) {
 					synchronized (s.getSessionMutexObject()) {
-						session = (SimpleSessionContext) s.getAttribute("com.dentsusoken.mtp.sessionContext." + tenantId);
+						session = (SimpleSessionContext) s.getAttribute("org.iplass.mtp.sessionContext." + tenantId);
 						if (session == null) {
 							session = new SimpleSessionContext();
-							s.setAttribute("com.dentsusoken.mtp.sessionContext." + tenantId, session);
+							s.setAttribute("org.iplass.mtp.sessionContext." + tenantId, session);
 						}
 					}
 				}
