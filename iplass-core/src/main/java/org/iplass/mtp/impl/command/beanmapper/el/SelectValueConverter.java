fd556b0c1d3a023cb2d8f3d57205e38ea8e70d71
xsekiguchi <xsekiguchi.naoya2@contract.dentsusoken.com>
2024-03-26 21:32:30 +0900

    ソースコード修正 jakarta el インターフェース変更

--- iplass-core/src/main/java/org/iplass/mtp/impl/command/beanmapper/el/SelectValueConverter.java
+++ iplass-core/src/main/java/org/iplass/mtp/impl/command/beanmapper/el/SelectValueConverter.java
@@ -1,40 +1,40 @@
 /*
  * Copyright (C) 2017 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package org.iplass.mtp.impl.command.beanmapper.el;
 
+import org.iplass.mtp.entity.SelectValue;
+
 import jakarta.el.ELContext;
 import jakarta.el.TypeConverter;
-
-import org.iplass.mtp.entity.SelectValue;
 
 public class SelectValueConverter extends TypeConverter {
 
 	@Override
-	public Object convertToType(ELContext context, Object obj, Class<?> targetType) {
+	public <T> T convertToType(ELContext context, Object obj, Class<T> targetType) {
 		if (SelectValue.class == targetType) {
 			if (obj instanceof String) {
 				if ("".equals(obj)) {
 					return null;
 				}
 				context.setPropertyResolved(true);
-				return new SelectValue(obj.toString());
+				return (T) new SelectValue(obj.toString());
 			}
 		}
 		return null;
