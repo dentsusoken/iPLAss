92d6e857fbf77f1e5031b103162c0ed888f2c5f6
xsekiguchi <xsekiguchi.naoya2@contract.dentsusoken.com>
2024-03-26 21:31:47 +0900

    ソースコード修正 admin プロジェクト fileupload servlet6 対応

--- iplass-admin/src/main/java/org/iplass/adminconsole/server/base/io/upload/CommonsFileuploadMultipartRequestParameterParser.java
+++ iplass-admin/src/main/java/org/iplass/adminconsole/server/base/io/upload/CommonsFileuploadMultipartRequestParameterParser.java
@@ -29,7 +29,7 @@ import org.apache.commons.fileupload2.core.DiskFileItemFactory;
 import org.apache.commons.fileupload2.core.FileItem;
 import org.apache.commons.fileupload2.core.FileItemFactory;
 import org.apache.commons.fileupload2.core.FileUploadException;
-import org.apache.commons.fileupload2.jakarta.servlet5.JakartaServletFileUpload;
+import org.apache.commons.fileupload2.jakarta.servlet6.JakartaServletFileUpload;
 
 import jakarta.servlet.http.HttpServletRequest;
 
