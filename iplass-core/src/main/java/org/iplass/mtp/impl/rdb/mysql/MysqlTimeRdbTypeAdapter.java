d35b74a091f1acc8e541863a112cbe7ed10356df
HiguchiKiyoshi <higuchi.kiyoshi@dentsusoken.com>
2024-04-25 19:40:28 +0900

    fix #1471 mysqlへの対応

---
+++ iplass-core/src/main/java/org/iplass/mtp/impl/rdb/mysql/MysqlTimeRdbTypeAdapter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 DENTSU SOKEN INC. All Rights Reserved.
+ * 
+ * Unless you have purchased a commercial license,
+ * the following license terms apply:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.iplass.mtp.impl.rdb.mysql;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import org.iplass.mtp.impl.entity.property.PropertyType;
+import org.iplass.mtp.impl.rdb.adapter.BaseRdbTypeAdapter;
+import org.iplass.mtp.impl.rdb.adapter.RdbAdapter;
+
+/**
+ * PreparedStatementのsetTimeでTimestamp型カラムに保存するためのAdapter。
+ * 
+ * @author K.Higuchi
+ *
+ */
+public class MysqlTimeRdbTypeAdapter extends BaseRdbTypeAdapter.Time {
+
+	public MysqlTimeRdbTypeAdapter(PropertyType propertyType) {
+		super(propertyType);
+	}
+
+	@Override
+	public void setParameter(int index, Object javaTypeValue,
+			PreparedStatement stmt, RdbAdapter rdb) throws SQLException {
+		java.sql.Time val = toRdb(javaTypeValue, rdb);
+		if (val != null) {
+			//日付部分を切り捨て
+			SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
+			val = java.sql.Time.valueOf(fmt.format(val));
+			stmt.setTimestamp(index, new Timestamp(val.getTime()));
+		} else {
+			stmt.setTimestamp(index, null);
+		}
+	}
+
+}
