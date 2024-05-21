d75369bd5953a14e548f77f62946f87ba6781cd7
Yukio Kazama <xkazama.yukio3@contract.dentsusoken.com>
2024-05-21 19:13:39 +0900

    fix #1490

--- iplass-core/src/main/java/org/iplass/mtp/impl/webhook/DefaultWebhookResponseHandler.java
+++ iplass-core/src/main/java/org/iplass/mtp/impl/webhook/DefaultWebhookResponseHandler.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (C) 2020 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package org.iplass.mtp.impl.webhook;
 
-import org.iplass.mtp.webhook.WebhookResponseHandler;
 import org.iplass.mtp.webhook.WebhookException;
 import org.iplass.mtp.webhook.WebhookHeader;
 import org.iplass.mtp.webhook.WebhookResponse;
+import org.iplass.mtp.webhook.WebhookResponseHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,8 +30,8 @@ public class DefaultWebhookResponseHandler implements WebhookResponseHandler{
 	private static Logger logger =  LoggerFactory.getLogger(DefaultWebhookResponseHandler.class);
 	@Override
 	public void handleResponse(WebhookResponse response) {
-		if (200<=response.getStatusCode()||response.getStatusCode()<300) {
-			writeLog(response);	
+		if (200 <= response.getStatusCode() && response.getStatusCode() < 300) {
+			writeLog(response);
 		} else {
 			throw new WebhookException("UnHandled Response Status Code: "+response.getStatusCode()+"("+response.getReasonPhrase()+")");
 		}
@@ -39,15 +39,15 @@ public class DefaultWebhookResponseHandler implements WebhookResponseHandler{
 
 	private void writeLog(WebhookResponse response) {
 		if (logger.isDebugEnabled()) {
-	        StringBuilder msg = new StringBuilder(); 
+	        StringBuilder msg = new StringBuilder();
 	        makeStatusString(response, msg);
 	        makeHeaderString(response, msg);
 	        makeEntityString(response, msg);
-		
+
 			logger.debug(msg.toString());
 		}
 	}
-	
+
 	private void makeStatusString(WebhookResponse whr, StringBuilder msg) {
 		msg.append("Response Code ");
 		msg.append(whr.getStatusCode());
