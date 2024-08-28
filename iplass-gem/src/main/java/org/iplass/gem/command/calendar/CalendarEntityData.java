84e2ca895977cb6c5e1ea421197caf9ae904c289
Yuma Ishida <51354834+yumaishida@users.noreply.github.com>
2024-08-28 12:53:12 +0900

    GEM: Calendar定義が既に存在していた場合のNull考慮漏れ対応 (#1609)

--- iplass-gem/src/main/java/org/iplass/gem/command/calendar/CalendarEntityData.java
+++ iplass-gem/src/main/java/org/iplass/gem/command/calendar/CalendarEntityData.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2012 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,7 @@ import org.iplass.mtp.view.calendar.EntityCalendarItem;
 
 /**
  * カレンダーに表示するEntityのデータ
+ *
  * @author lis3wg
  */
 public class CalendarEntityData {
@@ -57,9 +58,10 @@ public class CalendarEntityData {
 
 	/**
 	 * コンストラクタ
+	 *
 	 * @param entity Entity
-	 * @param time 時間
-	 * @param item カレンダーに表示するEntityの定義
+	 * @param time   時間
+	 * @param item   カレンダーに表示するEntityの定義
 	 */
 	public CalendarEntityData(Entity entity, String time, EntityCalendarItem item) {
 		this.defName = entity.getDefinitionName();
@@ -69,12 +71,15 @@ public class CalendarEntityData {
 		this.time = time;
 		this.dispTime = item.getDisplayTime();
 		this.viewAction = item.getViewAction();
-		this.allowNewRecordRegistration = item.getAllowNewRecordRegistration();
+		this.allowNewRecordRegistration = item.getAllowNewRecordRegistration() != null
+				? item.getAllowNewRecordRegistration()
+				: true;
 		this.viewName = item.getViewName();
 	}
 
 	/**
 	 * Entity定義名を取得します。
+	 *
 	 * @return Entity定義名
 	 */
 	public String getDefName() {
@@ -83,6 +88,7 @@ public class CalendarEntityData {
 
 	/**
 	 * Entity定義名を設定します。
+	 *
 	 * @param defName Entity定義名
 	 */
 	public void setDefName(String defName) {
@@ -91,6 +97,7 @@ public class CalendarEntityData {
 
 	/**
 	 * OIDを取得します。
+	 *
 	 * @return OID
 	 */
 	public String getOid() {
@@ -99,6 +106,7 @@ public class CalendarEntityData {
 
 	/**
 	 * OIDを設定します。
+	 *
 	 * @param oid OID
 	 */
 	public void setOid(String oid) {
@@ -107,22 +115,25 @@ public class CalendarEntityData {
 
 	/**
 	 * バージョンを取得します。
+	 *
 	 * @return バージョン
 	 */
 	public Long getVersion() {
-	    return version;
+		return version;
 	}
 
 	/**
 	 * バージョンを設定します。
+	 *
 	 * @param version バージョン
 	 */
 	public void setVersion(Long version) {
-	    this.version = version;
+		this.version = version;
 	}
 
 	/**
 	 * 表示用の名前を取得します。
+	 *
 	 * @return 表示用の名前
 	 */
 	public String getName() {
@@ -131,6 +142,7 @@ public class CalendarEntityData {
 
 	/**
 	 * 表示用の名前を設定します。
+	 *
 	 * @param name 表示用の名前
 	 */
 	public void setName(String name) {
@@ -139,38 +151,43 @@ public class CalendarEntityData {
 
 	/**
 	 * 時間を取得します。
+	 *
 	 * @return 時間
 	 */
 	public String getTime() {
-	    return time;
+		return time;
 	}
 
 	/**
 	 * 時間を設定します。
+	 *
 	 * @param time 時間
 	 */
 	public void setTime(String time) {
-	    this.time = time;
+		this.time = time;
 	}
 
 	/**
 	 * 時間を表示するかを取得します。
+	 *
 	 * @return 時間を表示するか
 	 */
 	public boolean isDispTime() {
-	    return dispTime;
+		return dispTime;
 	}
 
 	/**
 	 * 時間を表示するかを設定します。
+	 *
 	 * @param dispTime 時間を表示するか
 	 */
 	public void setDispTime(boolean dispTime) {
-	    this.dispTime = dispTime;
+		this.dispTime = dispTime;
 	}
 
 	/**
 	 * 表示用のアクションを取得します。
+	 *
 	 * @return 表示用のアクション
 	 */
 	public String getViewAction() {
@@ -179,6 +196,7 @@ public class CalendarEntityData {
 
 	/**
 	 * 表示用のアクションを設定します。
+	 *
 	 * @param viewAction 表示用のアクション
 	 */
 	public void setViewAction(String viewAction) {
@@ -187,22 +205,25 @@ public class CalendarEntityData {
 
 	/**
 	 * レコードの新規登録を許可するかを取得します。
+	 *
 	 * @return レコードの新規登録を許可するか
 	 */
 	public boolean getAllowNewRecordRegistration() {
-	    return allowNewRecordRegistration;
+		return allowNewRecordRegistration;
 	}
 
 	/**
 	 * レコードの新規登録を許可するかを設定します。
+	 *
 	 * @param allowNewRecordRegistration レコードの新規登録を許可するか
 	 */
 	public void setAllowNewRecordRegistration(boolean allowNewRecordRegistration) {
-	    this.allowNewRecordRegistration = allowNewRecordRegistration;
+		this.allowNewRecordRegistration = allowNewRecordRegistration;
 	}
 
 	/**
 	 * ビュー名を取得します。
+	 *
 	 * @return ビュー名
 	 */
 	public String getViewName() {
@@ -211,6 +232,7 @@ public class CalendarEntityData {
 
 	/**
 	 * ビュー名を設定します。
+	 *
 	 * @param viewName ビュー名
 	 */
 	public void setViewName(String viewName) {
