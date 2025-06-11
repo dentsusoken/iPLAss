a63fde3d4f83e7fc58ffda799cb49580d2f4fd80
xsekiguchi <xsekiguchi.naoya2@contract.dentsusoken.com>
2025-06-11 11:43:12 +0900

    CIエラー調査用のデバッグコードを追加（2025-06-11修正）

--- iplass-web/src/main/java/org/iplass/mtp/impl/auth/oauth/token/opaque/SameTokenCreationStrategy.java
+++ iplass-web/src/main/java/org/iplass/mtp/impl/auth/oauth/token/opaque/SameTokenCreationStrategy.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2019 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -39,14 +39,14 @@ import org.slf4j.LoggerFactory;
  * AuthTokenHandlerでは、tokenのhash設定はできない。
  * 同一Clientの同一リソースオーナーの複数端末からTokenリクエストがあっても、
  * いずれのリクエストにも有効な（同一の）Tokenを返すことができる。
- * 
+ *
  * @author K.Higuchi
  *
  */
 public class SameTokenCreationStrategy implements TokenCreationStrategy {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(SameTokenCreationStrategy.class);
-	
+
 	private long retryIntervalMillis;
 	private int retryCount;
 
@@ -69,17 +69,17 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 	@Override
 	public AuthToken create(OAuthClientRuntime client, AccessTokenHandler handler, String userUniqueId, AccessTokenInfo accessTokenInfo) {
 		//並列でコールされうることを考慮してリトライする
-		
+
 		if (handler.getHashSettings() != null && handler.getHashSettings().size() > 0) {
 			throw new SystemException("SameTokenCreationStrategy is not support AuthToken hash setting.");
 		}
-		
+
 		int tenantId = ExecuteContext.getCurrentContext().getClientTenantId();
 		String series = handler.newSeriesString(userUniqueId, null, accessTokenInfo);
-		
+
 		for (int i = 0; i <= retryCount; i++) {
 			AuthToken at = null;
-			
+
 			try {
 				at = Transaction.requiresNew(t -> {
 					AuthToken storeToken = handler.authTokenStore().getBySeries(tenantId, handler.getType(), series);
@@ -89,16 +89,16 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 						handler.authTokenStore().create(newToken);
 						return newToken;
 					}
-					
+
 					//新規のscopeが含まれている場合は作り直し
 					AccessTokenMement atm = (AccessTokenMement) storeToken.getDetails();
-					
+
 					//念のためハッシュ衝突をチェック
 					if (!atm.getResouceOwnerId().equals(userUniqueId)
 							|| !client.getMetaData().getId().equals(atm.getClientMetaDataId())) {
 						throw new SystemException("AccessToken's series hash may have collision: client=" + atm.getClientMetaDataId() + " ,series=" + series);
 					}
-					
+
 					if (atm.getGrantedScopes() == null || !atm.getGrantedScopes().containsAll(accessTokenInfo.getGrantedScopes())) {
 						HashSet<String> mergedScopes = new HashSet<>();
 						if (atm.getGrantedScopes() != null) {
@@ -108,15 +108,15 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 						AccessTokenInfo mergedAccessTokenInfo = new AccessTokenInfo();
 						mergedAccessTokenInfo.setClientName(accessTokenInfo.getClientName());
 						mergedAccessTokenInfo.setGrantedScopes(new ArrayList<>(mergedScopes));
-						
+
 						//delete/create new
 						handler.authTokenStore().deleteBySeries(tenantId, handler.getType(), series);
 						AuthToken newToken = handler.newAuthToken(userUniqueId, null, mergedAccessTokenInfo);
 						handler.authTokenStore().create(newToken);
 						return newToken;
 					}
-					
-					
+
+
 					//scopeが含まれている、且つ有効期限内であれば、同一のトークンを返却
 					AuthToken retToken = null;
 					if (atm.getExpires() > System.currentTimeMillis()) {
@@ -130,7 +130,13 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 						handler.authTokenStore().update(newToken, storeToken);
 						retToken = newToken;
 					}
-					
+
+					if (null != retToken) {
+						logger.debug(
+								"=====  check variable 0. retToken.getType() = {}, retToken.getOwnerId() = {}, retToken.getSeries() = {}, retToken.getToken() = {}",
+								retToken.getType(), retToken.getOwnerId(), retToken.getSeries(), retToken.getToken());
+					}
+
 					//check and create refreshToken
 					AccessTokenMement retTokenMement = (AccessTokenMement) retToken.getDetails();
 					boolean needRefreshToken = false;
@@ -141,24 +147,47 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 							needRefreshToken = true;
 						}
 					}
-					
+
+					logger.debug("===== - check variable 1. needRefreshToken = {}, retTokenMement.getRefreshToken() = {}", needRefreshToken, retTokenMement.getRefreshToken());
+
 					if (needRefreshToken && retTokenMement.getRefreshToken() == null) {
 						retTokenMement.setRefreshToken(handler.refreshTokenHandler().authTokenStore().getBySeries(tenantId, handler.refreshTokenHandler().getType(), retToken.getSeries()));
 					}
-					
+
+					logger.debug("===== -- check variable 2. needRefreshToken = {}, retTokenMement.getRefreshToken() = {}", needRefreshToken,
+							retTokenMement.getRefreshToken());
+
 					if (needRefreshToken) {
+
+						long currentTimeMillis = System.currentTimeMillis();
+
+						logger.debug("===== --- check variable 3. retTokenMement.getRefreshToken() = {}", retTokenMement.getRefreshToken());
+						if (null != retTokenMement.getRefreshToken()) {
+							logger.debug(
+									"===== ---- check variable 4. ((RefreshTokenMement) retTokenMement.getRefreshToken().getDetails()).getExpires() = {}, currentTimeMillis = {}",
+									((RefreshTokenMement) retTokenMement.getRefreshToken().getDetails()).getExpires(), currentTimeMillis);
+						}
+
 						if (retTokenMement.getRefreshToken() == null
-								|| ((RefreshTokenMement) retTokenMement.getRefreshToken().getDetails()).getExpires() <= System.currentTimeMillis()) {
+								|| ((RefreshTokenMement) retTokenMement.getRefreshToken().getDetails()).getExpires() <= currentTimeMillis) {
 							handler.refreshTokenHandler().authTokenStore().deleteBySeries(tenantId, handler.refreshTokenHandler().getType(), retToken.getSeries());
 							AuthToken refreshToken = handler.refreshTokenHandler().newAuthToken(retToken.getOwnerId(), retToken.getPolicyName(), new RefreshTokenInfo(client.getMetaData().getName()));
 							handler.refreshTokenHandler().authTokenStore().create(refreshToken);
 							retTokenMement.setRefreshToken(refreshToken);
 						}
+
+						logger.debug("===== ----- check variable 5. retTokenMement.getRefreshToken() = {}", retTokenMement.getRefreshToken());
+					}
+
+					if (null != retToken) {
+						logger.debug(
+								"===== ------ check variable 6. retToken.getType() = {}, retToken.getOwnerId() = {}, retToken.getSeries() = {}, retToken.getToken() = {}",
+								retToken.getType(), retToken.getOwnerId(), retToken.getSeries(), retToken.getToken());
 					}
 
 					return retToken;
 				});
-				
+
 			} catch (RuntimeException e) {
 				if (i == retryCount) {
 					throw e;
@@ -166,11 +195,11 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 					logger.warn("AuthToken:" + series + " update failed, do re-try...", e);
 				}
 			}
-			
+
 			if (at != null) {
 				return at;
 			}
-			
+
 			try {
 				Thread.sleep(retryIntervalMillis);
 			} catch (InterruptedException e) {
@@ -184,10 +213,10 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 	public AuthToken create(OAuthClientRuntime client, AccessTokenHandler handler, OpaqueRefreshToken refreshToken) {
 		int tenantId = ExecuteContext.getCurrentContext().getClientTenantId();
 		String series = refreshToken.getSeries();
-		
+
 		for (int i = 0; i <= retryCount; i++) {
 			AuthToken at = null;
-			
+
 			try {
 				at = Transaction.requiresNew(t -> {
 					AuthToken storeToken = handler.authTokenStore().getBySeries(tenantId, handler.getType(), series);
@@ -196,12 +225,12 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 						return null;
 					}
 					AccessTokenMement atm = (AccessTokenMement) storeToken.getDetails();
-					
+
 					//念のためハッシュ衝突をチェック(clientのみ)
 					if (!client.getMetaData().getId().equals(atm.getClientMetaDataId())) {
 						throw new SystemException("AccessToken's series hash may have collision: client=" + atm.getClientMetaDataId() + " ,series=" + series);
 					}
-					
+
 					//別リクエストが更新した
 					if (atm.getExpires() > System.currentTimeMillis()) {
 						return storeToken;
@@ -212,11 +241,11 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 						newAccessTokenInfo.setGrantedScopes(new ArrayList<>(atm.getGrantedScopes()));
 						AuthToken newToken = handler.newAuthToken(storeToken.getOwnerId(), null, newAccessTokenInfo);
 						handler.authTokenStore().update(newToken, storeToken);
-						
+
 						return newToken;
 					}
 				});
-		
+
 			} catch (RuntimeException e) {
 				if (i == retryCount) {
 					throw e;
@@ -227,7 +256,7 @@ public class SameTokenCreationStrategy implements TokenCreationStrategy {
 			if (at != null) {
 				return at;
 			}
-			
+
 			try {
 				Thread.sleep(retryIntervalMillis);
 			} catch (InterruptedException e) {
