55da272f42f29ea107af424a999bffbddf0e1f98
xsekiguchi <xsekiguchi.naoya2@contract.dentsusoken.com>
2025-08-25 19:31:40 +0900

    Apache Tika 3.2.2 バージョンアップ
    - (削除)org.apache.tika:tika-parser-zip-commons 2.9.2
    - (追加)org.apache.tika:tika-parser-text-module 3.2.2
    - org.apache.tika:tika-core 2.9.2 -> 3.2.2
    - org.apache.tika:tika-parser-html-module 2.9.2 -> 3.2.2
    - org.apache.tika:tika-parser-xml-module 2.9.2 -> 3.2.2
    - org.apache.tika:tika-parser-microsoft-module 2.9.2 -> 3.2.2
    - org.apache.tika:tika-parser-pdf-module 2.9.2 -> 3.2.2
    - org.apache.commons:commons-compress 1.26.2 -> 1.28.0
    - BinaryHtmlParserで利用するParserクラスを変更

--- iplass-core/src/main/java/org/iplass/mtp/impl/fulltextsearch/parser/BinaryHtmlParser.java
+++ iplass-core/src/main/java/org/iplass/mtp/impl/fulltextsearch/parser/BinaryHtmlParser.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2017 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -21,12 +21,12 @@
 package org.iplass.mtp.impl.fulltextsearch.parser;
 
 import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.parser.html.JSoupParser;
 
 /**
  * <p>
  * BinaryReferenceのコンテンツ解析Parser。
- * tikaのHtmlParserを利用してPDFの解析を行う。
+ * tikaのJSoupParserを利用してHTMLの解析を行う。
  * </p>
  */
 public class BinaryHtmlParser extends AbstractBinaryReferenceParser {
@@ -36,7 +36,6 @@ public class BinaryHtmlParser extends AbstractBinaryReferenceParser {
 
 	@Override
 	protected Parser parserInstance() {
-		return new HtmlParser();
+		return new JSoupParser();
 	}
-
 }
