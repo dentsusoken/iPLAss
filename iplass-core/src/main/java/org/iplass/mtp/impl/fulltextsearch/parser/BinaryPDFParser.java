0a41ea750fd263258998d1520baf8dbde3eac336
SEKIGUCHI Naoya <xsekiguchi.naoya2@contract.dentsusoken.com>
2025-08-26 18:53:48 +0900

    全文検索: BinaryPDFParserでXFAはパースしないようにする (#1837)
    - XFAパース対象外にする設定を追加

--- iplass-core/src/main/java/org/iplass/mtp/impl/fulltextsearch/parser/BinaryPDFParser.java
+++ iplass-core/src/main/java/org/iplass/mtp/impl/fulltextsearch/parser/BinaryPDFParser.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2017 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -33,6 +33,9 @@ import org.apache.tika.parser.pdf.PDFParser;
  * 内部で実行されるGrobidRESTParserはメタデータ情報を取得する目的のため、
  * Index生成用コンテンツ解析には不要。直接PDFParserを利用する。
  * </p>
+ * <p>
+ * XFAフォームの内容は抽出しません。
+ * </p>
  */
 public class BinaryPDFParser extends AbstractBinaryReferenceParser {
 
@@ -41,7 +44,10 @@ public class BinaryPDFParser extends AbstractBinaryReferenceParser {
 
 	@Override
 	protected Parser parserInstance() {
-		return new PDFParser();
+		PDFParser parser = new PDFParser();
+		parser.getPDFParserConfig().setExtractAcroFormContent(false);
+		parser.getPDFParserConfig().setIfXFAExtractOnlyXFA(false);
+		return parser;
 	}
 
 }
