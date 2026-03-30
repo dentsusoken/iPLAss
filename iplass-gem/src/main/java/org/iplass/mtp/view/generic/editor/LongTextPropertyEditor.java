04033225a84d47efcae7a9a0b1255c9405fc19e4
lismyr <lismyr@isidgp1937.snet.isid.co.jp>
2026-03-30 20:55:13 +0900

    指摘対応

--- iplass-gem/src/main/java/org/iplass/mtp/view/generic/editor/LongTextPropertyEditor.java
+++ iplass-gem/src/main/java/org/iplass/mtp/view/generic/editor/LongTextPropertyEditor.java
@@ -38,7 +38,7 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 		@Jsp(path = "/jsp/gem/generic/editor/StringPropertyEditor.jsp", key = ViewConst.DESIGN_TYPE_GEM),
 		@Jsp(path = "/jsp/gem/aggregation/unit/editor/StringPropertyEditor.jsp", key = ViewConst.DESIGN_TYPE_GEM_AGGREGATION),
 		@Jsp(
-				path = "/jsp/gem/SpreadSheet/element/section/editor/SpreadSheetViewFilterStringPropertyEditor.jsp",
+				path = "/jsp/gem/spreadsheet/element/section/editor/SpreadSheetViewFilterStringPropertyEditor.jsp",
 				key = ViewConst.DESIGN_TYPE_GEM_SPREADSHEETVIEW)
 })
 @IgnoreField({ "searchInRange", "hideSearchConditionFrom", "hideSearchConditionTo", "hideSearchConditionRangeSymbol", "searchExactMatchCondition" })
