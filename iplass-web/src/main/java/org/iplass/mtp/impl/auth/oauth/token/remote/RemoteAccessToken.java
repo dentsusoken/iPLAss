331cd2d21da0263a5670f2aa0f9587ed4efb73ac
SEKIGUCHI Naoya <xsekiguchi.naoya2@contract.dentsusoken.com>
2026-06-10 16:59:07 +0900

    AccessToken, RefreshToken に Serializable を実装（4.0） (#2085)
    - AccessToken, RefreshToken に Serializable を実装
    - 派生クラスの RemoteAccessToken, OpaqueAccessToken, OpaqueRefreshToken に serialVersionUID を実装

--- iplass-web/src/main/java/org/iplass/mtp/impl/auth/oauth/token/remote/RemoteAccessToken.java
+++ iplass-web/src/main/java/org/iplass/mtp/impl/auth/oauth/token/remote/RemoteAccessToken.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2019 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -26,7 +26,14 @@ import org.iplass.mtp.auth.User;
 import org.iplass.mtp.impl.auth.oauth.token.AccessToken;
 import org.iplass.mtp.impl.auth.oauth.token.RefreshToken;
 
+/**
+ * RemoteAccessToken
+ *
+ * @author DENTSU SOKEN INC.
+ */
 public class RemoteAccessToken extends AccessToken {
+	/** SerialVersionUID */
+	private static final long serialVersionUID = 5425823644475667735L;
 
 	private String tokenEncoded;
 	private User user;
