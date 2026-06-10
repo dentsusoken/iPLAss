4902a47d97f97b97cdc739a492db75a4220206ab
xsekiguchi <xsekiguchi.naoya2@contract.dentsusoken.com>
2026-06-10 21:08:04 +0900

    spotless適用

--- iplass-web/src/main/java/org/iplass/mtp/impl/webapi/rest/RestRequestContext.java
+++ iplass-web/src/main/java/org/iplass/mtp/impl/webapi/rest/RestRequestContext.java
@@ -19,11 +19,6 @@
  */
 package org.iplass.mtp.impl.webapi.rest;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.HttpMethod;
-import jakarta.ws.rs.core.Request;
-
 import org.iplass.mtp.impl.auth.authenticate.token.web.BearerTokenSupplier;
 import org.iplass.mtp.impl.web.WebRequestContext;
 import org.iplass.mtp.impl.web.WebRequestStack;
@@ -31,6 +26,11 @@ import org.iplass.mtp.web.WebRequestConstants;
 import org.iplass.mtp.webapi.WebApiRequestConstants;
 import org.iplass.mtp.webapi.definition.MethodType;
 import org.iplass.mtp.webapi.definition.RequestType;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.HttpMethod;
+import jakarta.ws.rs.core.Request;
 
 public class RestRequestContext extends WebRequestContext implements BearerTokenSupplier {
 	static final String WEB_API_RUNTIME_NAME = "mtp.restRequestContext.webApiRuntime";;
