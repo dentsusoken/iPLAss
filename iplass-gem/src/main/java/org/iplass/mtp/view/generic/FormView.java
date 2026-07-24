0755f0a4890ba56d471bb669c9f0f4310b869561
Hironari Kondoh <180364958+hkondoh@users.noreply.github.com>
2026-07-24 15:44:27 +0900

    フォーマットエラー修正

--- iplass-gem/src/main/java/org/iplass/mtp/view/generic/FormView.java
+++ iplass-gem/src/main/java/org/iplass/mtp/view/generic/FormView.java
@@ -33,7 +33,6 @@ import org.iplass.mtp.view.generic.element.Button;
 import org.iplass.mtp.view.generic.element.section.Section;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlSeeAlso;
