6edf87b4db73edd3efe2b970c76edac0e89cea88
SEKIGUCHI Naoya <xsekiguchi.naoya2@contract.dentsusoken.com>
2026-09-14 15:41:16 +0900

    Admin Console: EntityWebApi, TopView Fulltext Search 画面のグリッドの編集ができない (#2232)
    - グリッド編集可能にするための共通ヘルパーメソッドを追加
    - EntityWebApiDS, CrawlEntityDS でヘルパーメソッドを呼び出し

--- iplass-admin/src/main/java/org/iplass/adminconsole/client/base/data/AbstractAdminDataSource.java
+++ iplass-admin/src/main/java/org/iplass/adminconsole/client/base/data/AbstractAdminDataSource.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2017 DENTSU SOKEN INC. All Rights Reserved.
- * 
+ *
  * Unless you have purchased a commercial license,
  * the following license terms apply:
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -22,11 +22,14 @@ package org.iplass.adminconsole.client.base.data;
 
 import org.iplass.adminconsole.shared.base.dto.AdminUncaughtException;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.smartgwt.client.data.DSRequest;
 import com.smartgwt.client.data.DSResponse;
 import com.smartgwt.client.data.DataSource;
 import com.smartgwt.client.types.DSDataFormat;
 import com.smartgwt.client.types.DSProtocol;
+import com.smartgwt.client.util.JSOHelper;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
 
 public abstract class AbstractAdminDataSource extends DataSource {
 
@@ -75,4 +78,21 @@ public abstract class AbstractAdminDataSource extends DataSource {
 		throw new AdminUncaughtException("Unsupported operation.");
 	}
 
+	/**
+	 * グリッドレコードのレスポンスを更新する
+	 * <p>
+	 * request.getData()は変更項目のみのデータで届くため、キャッシュ行を置換する前に oldValues へのマージを実施する。
+	 * </p>
+	 *
+	 * @param requestId リクエストID
+	 * @param request DSリクエスト
+	 * @param response DSレスポンス
+	 */
+	protected void updateGridRecordResponse(String requestId, DSRequest request, DSResponse response) {
+		JavaScriptObject record = JSOHelper.createObject();
+		JSOHelper.addProperties(record, request.getOldValues()
+				.getJsObj());
+		JSOHelper.addProperties(record, request.getData());
+		response.setData(new ListGridRecord(record));
+	}
 }
--- iplass-admin/src/main/java/org/iplass/adminconsole/client/metadata/data/entity/CrawlEntityDS.java
+++ iplass-admin/src/main/java/org/iplass/adminconsole/client/metadata/data/entity/CrawlEntityDS.java
@@ -100,12 +100,13 @@ public class CrawlEntityDS extends AbstractAdminDataSource {
 
 	@Override
 	protected void executeUpdate(String requestId, DSRequest request, DSResponse response) {
+		updateGridRecordResponse(requestId, request, response);
 		processResponse(requestId, response);
 	}
 
 	private List<ListGridRecord> createRecord(List<EntityDefinition> entities) {
 
-		List<ListGridRecord> list = new ArrayList<ListGridRecord>();
+		List<ListGridRecord> list = new ArrayList<>();
 
 		if (entities != null) {
 			for (EntityDefinition entity : entities) {
